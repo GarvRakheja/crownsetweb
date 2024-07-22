0c06015e43d3339f4e2513500a08a8416d54e812
GarvRakheja <128740873+GarvRakheja@users.noreply.github.com>
2024-07-22 12:34:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:["thecrownset.com"]
+    }
+};
+
+export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary-color':'#6754E9',
+        'bodyTextColor':'#615f5b'
+      }
+     
+    },
+  },
+  plugins: [],
+};
